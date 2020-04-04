1b1758619a9e8962debae801355a88624f4fb150
jemmanue <jemmanue@isi-mtl.com>
2020-04-04 15:11:05 -0400

    Commit before starting the lent and brought back function

--- LibraryApp/MainWindow.xaml.cs
+++ LibraryApp/MainWindow.xaml.cs
@@ -33,6 +33,10 @@ namespace LibraryApp
         {
             libraryMembersGrid.ItemsSource = null;
             libraryMembersGrid.ItemsSource = Library.allLibraryMember;
+
+            mediaGrid.ItemsSource = null;
+            mediaGrid.ItemsSource = Library.AllMedia;
+
         }
 
         private void LendMedia_Click(object sender, RoutedEventArgs e)
@@ -49,6 +53,47 @@ namespace LibraryApp
             }
             var message = ((Media)mediaGrid.SelectedItem).MediaInfo();
             MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void MemberInfo_Click(object sender, RoutedEventArgs e)
+        {
+            if (libraryMembersGrid.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a Member to view more detailed Info.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            var message = ((LibraryMember)libraryMembersGrid.SelectedItem).MemberInfo();
+            MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void ModifyMember_Click(object sender, RoutedEventArgs e)
+        {
+            ModifyMemberPage modifyMemberPage;
+            if(((LibraryMember)libraryMembersGrid.SelectedItem)!=null)
+            {
+               modifyMemberPage=new ModifyMemberPage((LibraryMember)(libraryMembersGrid.SelectedItem),this.Content);
+                this.Content = modifyMemberPage;
+            }
+            else
+            {
+                MessageBox.Show("Please select a Member to  change  their Info.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+        }
+
+        private void ModifyMedia_Click(object sender, RoutedEventArgs e)
+        {
+            ModifyMediaPage modifyMediaPage;
+            if (((Media)mediaGrid.SelectedItem) != null)
+            {
+                modifyMediaPage = new ModifyMediaPage((Media)(mediaGrid.SelectedItem), this.Content);
+                this.Content = modifyMediaPage;
+            }
+            else
+            {
+                MessageBox.Show("Please select a library item to  change  their Info.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
         }
     }
 }
---
+++ LibraryApp/ModifyMemberPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryApp
+{
+    /// <summary>
+    /// Interaction logic for ModifyMemberPage.xaml
+    /// </summary>
+    public partial class ModifyMemberPage : Page
+    {
+        LibraryMember toModify;
+        object previouscontent;
+        public ModifyMemberPage(LibraryMember _toModify, object _previousContent)
+        {
+            InitializeComponent();
+            toModify = _toModify;
+            previouscontent = _previousContent;
+            SetInitialValues();
+        }
+        private void SetInitialValues()
+        {
+            fullNameBox.Text = toModify.Name;
+        }
+        private void SubmitRecord_Click(object sender, RoutedEventArgs e)
+        {
+            toModify.ModifyField(fullNameBox.Text);
+            ((MainWindow)(Application.Current.MainWindow)).RefreshDataGrid();
+            Application.Current.MainWindow.Content = previouscontent;
+        }
+       
+    }
+}
